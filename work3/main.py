ac1a3e4efff9d1a063adbf0cab0aaaa0958c0be5
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2024-12-01 13:03:35 +0100

    Update main.py

--- work3/main.py
+++ work3/main.py
@@ -1,9 +1,18 @@
 import sys
 from pathlib import Path
-from colorama import Fore
+#from colorama import Fore
+
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
 
 def view_directory(directory, indent=0):
     path = Path(directory)
+    print()
     pass
 
 view_directory('path')
